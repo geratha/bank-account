1305f78e044a6f6569d1aa43b00bb177ff0f81d3
Geratha <kandenzi018@gmail.com>
2019-01-19 11:00:43 +0200

    js folder

---
+++ js/script.js
@@ -0,0 +1,48 @@
+function bankAccount( firstName, secondName, otherNames, id, password, initialAmount, amountDeposit, amountWithdrow, acountBalance){
+    this.firstName=name;
+    this.secondName=secondName;
+    this.otherNames=otherNames;
+    this.id=number;
+    this.password=Number;
+    this.initialAmount=Amount;
+    this.amountDeposit=addedamount;
+    this.amountWithdrow=lemovedamount;
+    this.acountBalance=closingbalance;
+}
+// registration
+bankAccount.prototype.firstName=function(firstName){
+    this.firstName(name)}
+
+
+    bankAccount.prototype.secondName=function(secondName){
+        this.secondName(name)}
+
+        bankAccount.prototype.otherNames=function(otherNames){
+            this.otherNames(names)}
+
+            bankAccount.prototype.id=function(id){
+                this.id(number)}
+
+                bankAccount.prototype.password=function(password){
+                    this.password(number)}
+
+                    bankAccount.prototype.initialAmount=function(initialAmount){
+                        this.initialAmount(activeAmount)}
+
+                                bankAccount.prototype.acountBalance=function(acountBalance){
+                                    this.acountBalance(closingbalance)}
+
+//deposit transacion
+
+bankAccount.prototype.amountDeposit =function(addedamount){
+    this.amountDeposit +=(deposit)
+}
+//deposit transaction
+bankAccount.prototype.amountWithdrow=function(withdrow){
+    this.amountWithdrow -=(lemovedamount)}
+
+
+
+    //user interface
+    
+        
