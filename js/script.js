6b8adc7c84adb080ecd7dc8210f3cd6b63a14800
Geratha <kandenzi018@gmail.com>
2019-01-19 10:56:41 +0200

    index.html

---
+++ js/script.js
@@ -0,0 +1,71 @@
+function bankAccount( firstName, secondName, otherNames, id, password, initialAmount, amountDeposit, amountWithdrow, acountBalance){
+    this.firstName=name;
+    this.secondName=secondName;
+    this.otherNames=otherNames;
+    this.id=number;
+    this.password=Number;
+    this.initialAmount=Amount;
+    this.amountDeposit=addedamount;
+    this.amountWithdrow=lemovedamount;
+    this.acountBalance=closingbalance;
+}
+// registration
+bankAccount.prototype.firstName=function(firstName){
+    this.firstName(name)}
+
+
+    bankAccount.prototype.secondName=function(secondName){
+        this.secondName(name)}
+
+        bankAccount.prototype.otherNames=function(otherNames){
+            this.otherNames(names)}
+
+            bankAccount.prototype.id=function(id){
+                this.id(number)}
+
+                bankAccount.prototype.password=function(password){
+                    this.password(number)}
+
+                    bankAccount.prototype.initialAmount=function(initialAmount){
+                        this.initialAmount(activeAmount)}
+
+                                bankAccount.prototype.acountBalance=function(acountBalance){
+                                    this.acountBalance(closingbalance)}
+
+//deposit transacion
+
+bankAccount.prototype.amountDeposit =function(addedamount){
+    this.amountDeposit +=(deposit)
+}
+//deposit transaction
+bankAccount.prototype.amountWithdrow=function(withdrow){
+    this.amountWithdrow -=(lemovedamount)}
+
+
+
+    //user interface
+    
+        $(document).ready(function() {
+        var newAccount = Object.create(BankAccount);
+        $("form#create").submit(function(event) {
+        event.preventDefault();
+        var inputtedName = $("input#name").val();
+        var inputtedBalance = parseInt($("input#initial").val());
+        newAccount.name = inputtedName;
+        newAccount.deposit(inputtedBalance);
+        $(".balance").text(newAccount.balance);
+        });
+        $("form#manage").submit(function(event) {
+        event.preventDefault();
+        var newBalance = parseInt($("input#deposit").val());
+        newAccount.deposit(newBalance);
+        $(".balance").text(newAccount.balance);
+        // $("#manage").unbind('submit');
+        });
+        $("form#withdraw").submit(function(event) {
+        event.preventDefault();
+        var newBalance = parseInt($("input#withdraw").val());
+        newAccount.withdraw(newBalance);
+        $(".balance").text(newAccount.balance);
+        });
+        });
